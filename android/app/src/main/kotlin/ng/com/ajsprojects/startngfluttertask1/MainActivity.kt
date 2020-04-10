d5a1956d3d17cc0c67bfc8be3c647c369095439a
Odujole Ajibola <odujole.damilare@gmail.com>
2020-04-10 11:26:02 +0100

    Initial commit

---
+++ android/app/src/main/kotlin/ng/com/ajsprojects/startngfluttertask1/MainActivity.kt
@@ -0,0 +1,6 @@
+package ng.com.ajsprojects.startngfluttertask1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Center(
+        child: Text(
+            "Full Name: Ajibola Odujole\nSlack Username: Joules007\nTracks: Mobile & Design"),
+      ),
+    ),
+  );
+}
